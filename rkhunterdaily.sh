e7567338c2cb3c8b091c43cc931c8eddfd4ec82f
Ladislav Grulich (LaGr77) <lada.grulich@gmail.com>
2022-01-19 23:34:57 +0100

    2022-01-19 23:34

---
+++ rkhunterdaily.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# written by Ladislav Grulich (LaGr77)
+# version 0.1.0
+# since 2022-01-19
+# licence MIT
+
+#RKHUNTER_LOGFILE="/var/log/xxx/xxx-$(date +'%w').log"
+RKHUNTER_EMAIL="email-1@gmail.com"
+RKHUNTER_EMAIL_CC="email-2@gmail.com"
+RKHUNTER_EMAIL_FROM="rkHunter@$HOSTNAME"
