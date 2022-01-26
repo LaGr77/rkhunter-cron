1edf969086aa4f7e13aee2aea4ff20dc6483f511
Ladislav Grulich (LaGr77) <lada.grulich@gmail.com>
2022-01-26 23:12:48 +0100

    Try to run, but TODO: run as SU

--- rkhunterdaily.sh
+++ rkhunterdaily.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
 # written by Ladislav Grulich (LaGr77)
-# version 0.1.1
-# since 2022-01-20
+# version 0.1.2
+# since 2022-01-26
 # licence MIT
-
-#First TODO
-# nano /etc/rkhunter.conf
-# MIRRORS_MODE=1
-# MIRRORS_MODE=0
-# WEB_CMD=""
-
-# rkhunter -C
-
-# rkhunter --update
-
-# rkhunter --versioncheck
-
-# rkhunter --propupd
-
-# rkhunter --check
-# rkhunter --check --sk
-# rkhunter --check --report-warnings-only
-
-# nano /etc/rkhunter.conf
-# ALLOWHIDDENDIR=/etc/.java
 
 RKHUNTER_LOGFILE="/var/log/rkhunter/rkhunter-$(date +'%w').log"
 RKHUNTER_EMAIL="email-1@gmail.com"
 RKHUNTER_EMAIL_CC="email-2@gmail.com"
 RKHUNTER_EMAIL_FROM="rkHunter@{$HOSTNAME}"
 
-nice -n10 rkhunter --check --report-warnings-only  &>"${RKHUNTER_LOGFILE}";
-##--cronjob
+nice -n0 rkhunter --check --cronjob --report-warnings-only  &>"${RKHUNTER_LOGFILE}";
 
 (
     echo "--------- VERSION SUMMARY ----------"
