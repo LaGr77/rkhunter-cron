f4ee96711b87927c88f7f707259b93df28928a11
Laďa <67732401+LaGr77@users.noreply.github.com>
2022-06-17 12:06:04 +0200

    Update rkhunterdaily.sh
    CHANGE: Logs 2 variables

--- rkhunterdaily.sh
+++ rkhunterdaily.sh
@@ -4,13 +4,13 @@
 # since 2022-01-26
 # licence MIT
 
-RKHUNTER_LOGFILE="/var/log/rkhunter/rkhunter-$(date +'%w').log"
+#RKHUNTER_LOGFILE="/var/log/rkhunter/rkhunter-$(date +'%w').log"
 RKHUNTER_EMAIL="email-1@gmail.com"
 RKHUNTER_EMAIL_CC="email-2@gmail.com"
 RKHUNTER_EMAIL_FROM="rkHunter@{$HOSTNAME}"
 
-nice -n0 rkhunter --check --cronjob --report-warnings-only  &>"${RKHUNTER_LOGFILE}";
-
+#nice -n0 rkhunter --check --cronjob --report-warnings-only  &>"${RKHUNTER_LOGFILE}";
+RKHUNTER_MSG=$(nice -n10 rkhunter --check --cronjob --report-warnings-only)
 (
     echo "--------- VERSION SUMMARY ----------"
 	rkhunter --versioncheck
@@ -19,7 +19,8 @@ nice -n0 rkhunter --check --cronjob --report-warnings-only  &>"${RKHUNTER_LOGFIL
     rkhunter --update
     echo ""
     echo "----------- CHECK SYSTEM -----------"
-    cat "${RKHUNTER_LOGFILE}"
+    #cat "${RKHUNTER_LOGFILE}"
+    echo $RKHUNTER_MSG
 ) | mail -s "[RKHUNTER] ${HOSTNAME}" -r "${RKHUNTER_EMAIL_FROM}" "${RKHUNTER_EMAIL}","${RKHUNTER_EMAIL_CC}"
 
 
